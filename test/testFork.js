b692ef45973e8c48dd724f1da6de618c7737398c
mruefenacht <ruefe@rcky.ch>
2014-08-28 14:27:22 +0200

    Added fork

---
+++ test/testFork.js
@@ -0,0 +1,103 @@
+var   assert    = require('assert')
+    , types     = require('ee-types');
+
+var async = require('..');
+
+function asyncFunction(delay, err, result, callback){
+    setTimeout(function(){
+        callback(err, result);
+    }, delay);
+}
+
+function syncFunction(err, result, callback){
+    callback(err, result);
+}
+
+describe('Fork', function(){
+    describe('setup', function(){
+        var fork = async.fork(function(errors, results){});
+        it('should return a function on invocation', function(){
+            assert(types.function(fork));
+        });
+        it('which exposes its context for introspection', function(){
+            assert(fork._context);
+        });
+        it('the context is empty on setup', function(){
+            assert.equal(0, fork._context.expected);
+            assert.equal(0, fork._context.errors.length);
+            assert.equal(0, fork._context.results.length);
+        });
+    });
+    describe('execution', function(){
+        var err = new Error('Uh oh');
+        it('should invoke the join callback at the end', function(done){
+            var fork = async.fork(function(errors, results){
+                done();
+            });
+
+            asyncFunction(5, null, 100, fork());
+            asyncFunction(10, null, true, fork());
+            asyncFunction(8, err, null, fork());
+        });
+
+        it('should pass an equal amount of errors and data to the callback', function(done){
+            var fork = async.fork(function(errors, results){
+                assert.equal(errors.length, results.length);
+                assert.equal(3, results.length);
+                done();
+            });
+
+            asyncFunction(5, null, 100, fork());
+            asyncFunction(10, null, true, fork());
+            asyncFunction(8, err, null, fork());
+        });
+
+        it('containing adequate data', function(done){
+            var fork = async.fork(function(errors, results){
+                assert(errors[1] === err);
+                assert.equal(100, results[0]);
+                assert.equal(null, results[1]);
+                done();
+            });
+
+            asyncFunction(5, null, 100, fork());
+            asyncFunction(10, null, true, fork());
+            asyncFunction(8, err, null, fork());
+        });
+
+        it('should fulfill the same constraints for synchronous functions (ordering changes)', function(done){
+            var fork = async.fork(function(errors, results){
+
+                assert(errors[2] === err);
+                assert.equal(100, results[0]);
+                assert.equal(true, results[1]);
+                assert.equal(errors.length, results.length);
+                assert.equal(3, results.length);
+
+                done();
+            });
+
+            syncFunction(null, 100, fork());
+            syncFunction(null, true, fork());
+            syncFunction(err, null, fork());
+        });
+
+        it('should fulfill the same constraints for mixed type functions (ordering changes)', function(done){
+            var fork = async.fork(function(errors, results){
+
+                assert(errors[1] === err);
+                assert.equal(true, results[2]);
+                assert.equal(errors.length, results.length);
+                assert.equal(3, results.length);
+
+                done();
+            });
+
+
+            asyncFunction(10, null, true, fork());
+            asyncFunction(5, err, null, fork());
+            syncFunction(null, 100, fork());
+        });
+
+    });
+});
